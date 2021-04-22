463f348eced2ffe38c9ec8d72e068b55640c8e55
Matt Ross <mattross1080@gmail.com>
2021-04-22 19:12:16 -0400

    adds formating code day of show

--- csvToRedis.py
+++ csvToRedis.py
@@ -6,6 +6,7 @@ import sys
 REDIS_HOST = 'redis-14390.c256.us-east-1-2.ec2.cloud.redislabs.com'
 zipcode_data = {}
 
+
 def read_csv_data(csv_file):
     with open("ny_zc.csv") as csvf:
         print("loaded data")
@@ -22,42 +23,83 @@ def read_csv_data(csv_file):
                 # unemp_rate = row[7]
                 # median_income = row[13]
                 data_for_zipcode = {
-                    "zipcode" : row[0],
-                    "white_pct" : row[1],
-                    "black_pct" : row[2],
-                    "asian_pct" : row[3],
-                    "latino_pct" : row[6],
-                    "unemp_rate" : row[7],
-                    "commute_time" : row[8],
-                    "service_pct" : row[9],
-                    "construction_pct" : row[10],
-                    "finance_pct" : row[11],
-                    "maunfacturing_pct" : row[12],
-                    "median_income" : row[13],
-                    "health_insurance_pct" : row[14],
-                    "poverty_pct" : row[15]
+                    "zipcode": row[0],
+                    "white_pct": row[1],
+                    "black_pct": row[2],
+                    "asian_pct": row[3],
+                    "latino_pct": row[6],
+                    "unemp_rate": row[7],
+                    "commute_time": row[8],
+                    "service_pct": row[9],
+                    "construction_pct": row[10],
+                    "finance_pct": row[11],
+                    "maunfacturing_pct": row[12],
+                    "median_income": row[13],
+                    "health_insurance_pct": row[14],
+                    "poverty_pct": row[15]
                 }
 
                 data_points = [
-                    "{}% White Population".format(data_for_zipcode["white_pct"]),
-                    "{}% Black Population".format(data_for_zipcode["black_pct"]),
-                    "{}% Asian Population".format(data_for_zipcode["asian_pct"]),
-                    "{}% Latino Population".format(data_for_zipcode["latino_pct"]),
-                    "{} Unemployment Rate".format(data_for_zipcode["unemp_rate"]),
-                    "{} Average Commute Time".format(data_for_zipcode["commute_time"]),
-                    "{}% Work in the Service Industry".format(data_for_zipcode["service_pct"]),
-                    "{}% Work in the Construction Industry".format(data_for_zipcode["construction_pct"]),
-                    "{}% Work in the Finance Industry".format(data_for_zipcode["finance_pct"]),
-                    "{}% Work in the Manufacturing Industry".format(data_for_zipcode["maunfacturing_pct"]),
-                    "${} Median Income".format(data_for_zipcode["median_income"]),
-                    "{}% Have Health Insurance".format(data_for_zipcode["health_insurance_pct"]),
-                    "{}% Families live below the Poverty Line".format(data_for_zipcode["poverty_pct"]),
+                    "{}% White Population".format(
+                        data_for_zipcode["white_pct"]),
+                    "{}% Black Population".format(
+                        data_for_zipcode["black_pct"]),
+                    "{}% Asian Population".format(
+                        data_for_zipcode["asian_pct"]),
+                    "{}% Latino Population".format(
+                        data_for_zipcode["latino_pct"]),
+                    "{} Unemployment Rate".format(
+                        data_for_zipcode["unemp_rate"]),
+                    "{} Minutes Average Commute Time".format(
+                        data_for_zipcode["commute_time"]),
+                    "{}% Work in the Service Industry".format(
+                        data_for_zipcode["service_pct"]),
+                    "{}% Work in the Construction Industry".format(
+                        data_for_zipcode["construction_pct"]),
+                    "{}% Work in the Finance Industry".format(
+                        data_for_zipcode["finance_pct"]),
+                    "{}% Work in the Manufacturing Industry".format(
+                        data_for_zipcode["maunfacturing_pct"]),
+                    "${} Median Income".format(
+                        data_for_zipcode["median_income"]),
+                    "{}% Have Health Insurance".format(
+                        data_for_zipcode["health_insurance_pct"]),
+                    "{}% Families live below the Poverty Line".format(
+                        data_for_zipcode["poverty_pct"]),
                 ]
 
+                data_points_es = [
+                    "{}% población blanca".format(
+                        data_for_zipcode["white_pct"]),
+                    "{}% población negra".format(
+                        data_for_zipcode["black_pct"]),
+                    "{}% población asiática".format(
+                        data_for_zipcode["asian_pct"]),
+                    "{}% población latina".format(
+                        data_for_zipcode["latino_pct"]),
+                    "{}% tasa de desempleo".format(
+                        data_for_zipcode["unemp_rate"]),
+                    "{} minutos, tiempo promedio de viaje al trabajo".format(
+                        data_for_zipcode["commute_time"]),
+                    "{}% Trabaja en la industria de servicio".format(
+                        data_for_zipcode["service_pct"]),
+                    "{}% Trabaja en la industria de construcción".format(
+                        data_for_zipcode["construction_pct"]),
+                    "{}% Trabaja en la industria de construcción".format(
+                        data_for_zipcode["finance_pct"]),
+                    "{}% Trabaja en la industria de manufactura".format(
+                        data_for_zipcode["maunfacturing_pct"]),
+                    "${} Ingreso medio".format(
+                        data_for_zipcode["median_income"]),
+                    "{}% Tiene seguro médico".format(
+                        data_for_zipcode["health_insurance_pct"]),
+                    "{}% Familias que viven debajo del nivel de pobreza".format(
+                        data_for_zipcode["poverty_pct"]),
+                ]
 
-                zipcode_data[row[0]] = data_points
-                print(
-                    data_for_zipcode)
+                zipcode_data[row[0]] = {"en": data_points, "es":data_points_es}
+                # print(
+                #     data_for_zipcode)
                 line_count += 1
         print(f'Processed {line_count} lines.')
     with open('zipcode_data.txt', 'w') as outfile:
--- formatPrintout.py
+++ formatPrintout.py
@@ -14,28 +14,28 @@ def testPrint():
 answer_lookup = {
     "en": {
         "a1": {
-            "1": "Do",
-            "2": "Have",
-            "Yes!": "Do",
-            "No!": "Have"
+            "1": "do",
+            "2": "have",
+            "Yes!": "do",
+            "No!": "have"
         },
         "a2": {
-            "1": "Fully Implicated",
-            "2": "Modestly Implicated",
-            "3": "Slightly Implicated",
-            "4": "Not Implicated At All"
+            "1": "fully Implicated",
+            "2": "modestly implicated",
+            "3": "slightly implicated",
+            "4": "not implicated at all"
         },
         "a3": {
-            "1": "How you see and identify yourself",
-            "2": "How others see and identify you",
-            "3": "How you see yourself and how others see you",
+            "1": "how you see and identify yourself",
+            "2": "how others see and identify you",
+            "3": "how you see yourself and how others see you",
             "4": "nothing"
         },
         "sugarIntake": {
-            "1": "High",
-            "2": "Medium",
-            "3": "Low",
-            "4": "None"
+            "1": "high",
+            "2": "medium",
+            "3": "low",
+            "4": "none"
         }
     },
     "es": {
@@ -46,22 +46,22 @@ answer_lookup = {
             "No!": " no conoce y no ha oído"
         },
         "a2": {
-            "1": "Totalmente implicado",
-            "2": "Modestamente implicado",
-            "3": "Ligeramente implicado",
-            "4": "Para nada implicado"
+            "1": "totalmente implicado",
+            "2": "modestamente implicado",
+            "3": "ligeramente implicado",
+            "4": "para nada implicado"
         },
         "a3": {
-            "1": "Cómo te ves e identificas",
-            "2": "Como otres te ven e identifican",
-            "3": "Todo lo anterior",
-            "4": "Ninguna de las anteriores"
+            "1": "cómo te ves e identificas",
+            "2": "como otres te ven e identifican",
+            "3": "todo lo anterior",
+            "4": "ninguna de las anteriores"
         },
         "sugarIntake": {
-            "1": "Alto",
-            "2": "Medio",
-            "3": "Bajo",
-            "4": "Ninguno"
+            "1": "alto",
+            "2": "medio",
+            "3": "bajo",
+            "4": "ninguno"
         }
     }
 }
@@ -77,10 +77,10 @@ with open('/home/pi/zipcode_data.txt') as json_file:
 def formatDocument(userInfo):
     print("Startomg")
 
-    doc = Document("/home/pi/CivilReviewEN.docx")
+    doc = Document("/home/pi/CivilReviewENTemplate.docx")
     lang = userInfo["lang"]
     if userInfo["lang"] == "es":
-        doc = Document("/home/pi/CivilReviewES.docx")
+        doc = Document("/home/pi/CivilReviewESTemplate.docx")
 
     data_for_zip = zipcode_data_lookup[userInfo["zipcode"]]
     print(data_for_zip)
@@ -93,7 +93,7 @@ def formatDocument(userInfo):
 
     print("Creating Document using this info")
     print(userInfo)
-    random_data_points = random.sample(range(1, len(data_for_zip)), 3)
+    random_data_points = random.sample(range(1, len(data_for_zip["en"])), 3)
     print(random_data_points)
     for paragraph in doc.paragraphs:
         # print(paragraph.text)
@@ -105,63 +105,67 @@ def formatDocument(userInfo):
         if '[NAME]' in paragraph.text:
             paragraph.style = 'Insertion'
             paragraph.text = 'Applicant: {}'.format(userInfo["userName"])
+            if lang == "es":
+                paragraph.text = 'Solicitante: {}'.format(userInfo["userName"])
 
         if '[QUALIFYSTATUS]' in paragraph.text:
             paragraph.style = 'Insertion'
-            paragraph.text = 'Result : DISQUALIFY'
+            paragraph.text = 'Result : Disqualify'
+            if lang == "es":
+                paragraph.text = "Resultado : No Califica"
 
-        if '[Q1Part1]' in paragraph.text:
+        if '[Q1Part1]' in paragraph.text or "[Q1]" in paragraph.text:
             q1Answer = answer_lookup[lang]["a1"][userInfo["a1"]]
-            q1Part1Answer = "[Do]" if q1Answer == "1" else "[Don't]"
-            q1Part2Answer = "[Have]" if q1Answer == "1" else ""
+            q1Part1Answer = "[do]" if q1Answer == "1" else "[don't]"
+            q1Part2Answer = "[have]" if q1Answer == "1" else ""
 
 
             if lang == "en":
-                paragraph.text = "You {} know or have {} heard of any of the suspects and witnesses.  An impartial reviewer wouldn’t know or heard of the suspects and witnesses.".format(
+                paragraph.text = "You {} know or have {} heard of any of the suspects and witnesses.  To be an impartial reviewer you can not know or have heard of the suspects and witnesses.".format(
                     q1Part1Answer, q1Part2Answer)
             else:
                 q1Answer = answer_lookup[lang]["a1"][userInfo["a1"]]
-                paragraph.text = "Usted [{}] de los testigos o los sospechosos.".format(q1Answer)
+                paragraph.text = "Usted [{}] de los testigos o los sospechosos.  Para ser un evaluador imparcial, no puede ni conocer, ni haber oído hablar de los sospechosos y testigos.".format(q1Answer)
 
         if '[Q2]' in paragraph.text:
             questionTwoAnswer = answer_lookup[lang]["a2"][userInfo["a2"]]
 
-            paragraph.text = "You think the U.S. is [{}] in the racial constructs of the D.R..  An impartial would recognize that the U.S.A. is a cultural, economic, and political hegemonic imperial power.".format(
+            paragraph.text = "You think the U.S. is [{}] in the racial constructs of the D.R..  To be an impartial reviewer you would have to recognize that the U.S.A. is a cultural, economic, and political hegemonic imperial power.".format(
                 questionTwoAnswer)
             if lang == "es":
-                paragraph.text = "Usted cree que los Estados Unidos está [{}] en las construcciones raciales de R.D.".format(questionTwoAnswer)
+                paragraph.text = "Usted cree que los Estados Unidos está [{}] en las construcciones raciales de R.D..  Para ser un evaluador imparcial, tendría que reconocer que los Estados Unidos es una potencia imperial hegemónica cultural, económica y política.".format(questionTwoAnswer)
 
         if '[Q3]' in paragraph.text:
             questionThreeAnswer = answer_lookup[lang]["a3"][userInfo["a3"]]
 
-            paragraph.text = "For you, [{}] affects your material conditions.  An impartial reviewer will understand the 'othering' mechanisms as tools of control. ".format(questionThreeAnswer)
+            paragraph.text = "For you, [{}] affects your material conditions.  To be an impartial reviewer you would have to see 'othering' mechanisms as tools of control.  ".format(questionThreeAnswer)
             if lang == "es":
-                paragraph.text = "Para usted, [{}] afecta sus condiciones materiales.".format(questionThreeAnswer)
+                paragraph.text = "Para usted, [{}] afecta sus condiciones materiales.  Para ser un evaluador imparcial, tendría que ver los mecanismos 'de alteridad' como herramientas de control.".format(questionThreeAnswer)
 
         if '[Q4]' in paragraph.text:
             questionFourAnswer = answer_lookup[lang]["sugarIntake"][userInfo["sugarIntake"]]
-            paragraph.text = "Your weekly sugar intake is {}.  An impartial reviewer would not consume any sugar.".format(
+            paragraph.text = "Your weekly sugar intake is {}.  To be an impartial reviewer would not consume any sugar.".format(
                 answer_lookup[lang]["sugarIntake"][userInfo["sugarIntake"]])
             if lang == "es":
-                paragraph.text = "Su consumo semanal de azúcar es [{}].".format(questionFourAnswer)
+                paragraph.text = "Su consumo semanal de azúcar es [{}].  Para ser un evaluador imparcial, no debería consumir azúcar.".format(questionFourAnswer)
 
         if '[XX%]' in paragraph.text:
             print("Setting Random Value 1")
             paragraph.style = 'Insertion'
 
-            print(data_for_zip[random_data_points[0]])
-            paragraph.text = data_for_zip[random_data_points[0]]
+            # print(data_for_zip[random_data_points[0]])
+            paragraph.text = data_for_zip[lang][random_data_points[0]]
 
 
-        if '[$YY]' in paragraph.text:
+        if '[YY%]' in paragraph.text or '[$YY]' in paragraph.text:
             print("Setting Random Value 2")
             paragraph.style = 'Insertion'
-            paragraph.text = data_for_zip[random_data_points[1]]
+            paragraph.text = data_for_zip[lang][random_data_points[1]]
 
         if '[ZZ%]' in paragraph.text:
             print("Setting Random Value 3")
             paragraph.style = 'Insertion'
-            paragraph.text = data_for_zip[random_data_points[2]]
+            paragraph.text = data_for_zip[lang][random_data_points[2]]
 
     doc_name = '{}.docx'.format(userInfo["userName"])
     doc.save(doc_name)
@@ -174,7 +178,7 @@ def formatDocument(userInfo):
 
 if __name__ == "__main__":
     try:
-        formatDocument({'userName': 'sbb', 'userId': 'd63142d7cf6f53a093ebc32ae1448f18', 'a1': '1', 'a2': '1',
-                       'a3': '1', 'zipcode': '11222', 'sugarIntake': '1', 'archivePermission': '1', 'lang': 'es'})
+        formatDocument({'userName': 'Hubess', 'userId': 'd63142d7cf6f53a093ebc32ae1448f18', 'a1': '1', 'a2': '1',
+                       'a3': '1', 'zipcode': '11222', 'sugarIntake': '1', 'archivePermission': '1', 'lang': 'en'})
     except Exception as e:
         print(e)
